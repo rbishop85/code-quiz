66028d1c5425f9e828160eb6f0ce8b58fc80382b
Robert Bishop <rbishop85@gmail.com>
2022-06-14 19:35:49 -0700

    Setup basic timer countown that clears at 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var scoreTimeEl = document.querySelector("#timer");
 var quizEl = document.querySelector("#quiz");
 var answerResultsEl = document.querySelector("#answerresults");
 
-console.log(scoreTime);
+console.log(scoreTimeEl);
 console.log(quizEl);
 console.log(answerResultsEl);
 
@@ -36,8 +36,23 @@ var questions = [
         correctAnswer: "A"
     }
 ]
+countdown();
 
-
+// Countdown function pulled from day 1 activity 10
+function countdown() {
+    var timeLeft = 10;
+  
+    var timeInterval = setInterval(function () {
+      if (timeLeft >= 1) {
+        scoreTimeEl.textContent = timeLeft;
+        timeLeft--;
+      } else {
+        scoreTimeEl.textContent = "";
+        clearInterval(timeInterval);
+        // Call a function here if you want it to run when clock hits 0
+      }
+    }, 1000);
+  }
 
 
 
